48645ea3e2250e2939d7cf375cb9b496861a5fc1
apinod <angelmpino87@yahoo.com>
2015-07-25 02:07:08 -0400

    checkpoint

---
+++ app/views/layout/assets/css.blade.php
@@ -0,0 +1,67 @@
+
+<link rel="stylesheet" href="/packages/fonts/font-awesome-4.3.0/css/font-awesome.min.css">
+
+<!-- Styles -->
+
+<link rel="stylesheet" href="/packages/styles/adminica/reset.css">
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,700">
+
+<link rel="stylesheet" href="/packages/styles/plugins/bootstrap/bootstrap-2.3.1.css">
+<link rel="stylesheet" href="/packages/styles/plugins/all/plugins.css">
+<link rel="stylesheet" href="/packages/styles/adminica/all.css">
+
+{{-- <link rel="stylesheet" href="/packages/styles/themes/layout_fixed.css" > --}}
+<link rel="stylesheet" href="/packages/styles/themes/nav_top.css" >
+<link rel="stylesheet" href="/packages/styles/themes/skin_light.css">
+<link rel="stylesheet" href="/packages/styles/themes/theme_green.css" >
+<link rel="stylesheet" href="/packages/styles/adminica/colours.css">
+
+<style type="text/css">
+	fieldset .label_20 {
+		width: 20%!important;
+	}
+	fieldset .label_30 {
+		width: 30%!important;
+	}
+	fieldset .label_50 {
+		width: 50%!important;
+	}	
+	select {
+		width: 100%!important;
+		margin-bottom: 0px!important;
+	}	
+	input:not(.disabled) {
+		background-color: #ffffff!important;
+		border: 1px solid #cccccc!important;
+	}
+	#wrapper {
+		width: 100%!important;
+	}
+	.no-border {
+		border: none!important;
+	}
+	.padding-sides10 {
+		padding-left: 10px!important;
+		padding-right: 10px!important;		
+	}
+	.span-button {
+		float: left!important;
+	}
+	a.logo {
+	  background: url("/packages/images/interface/light_logo.png") no-repeat scroll 0 -0px transparent !important;
+	}
+	.space-10 {
+		margin: 10px;
+		overflow: hidden;
+	}
+	.space-20 {
+		margin: 20px;
+		overflow: hidden;		
+	}	
+	.menu-li {
+		padding-left: 8px!important;
+		padding-right: 8px!important;
+	}
+</style>
+
+@yield('append_css')
